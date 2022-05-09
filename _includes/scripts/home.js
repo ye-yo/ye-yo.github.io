a73ebabb277c75efcbd0713f4b1794a1434ba2ae
ye-yo <yeyo0x0@gmail.com>
2022-05-09 23:02:15 +0900

    Feat: 프로필 기능 추가 및 레이아웃 수정 Squashed commit of the following:
    commit 851d9a56097c5ad12ae5234ee78f98e429047e21
    Author: ye-yo <yeyo0x0@gmail.com>
    Date:   Mon May 9 23:00:48 2022 +0900
    
        Style: 컨텐츠 헤더 간격 및 아이콘, 문구 레이아웃 수정
    
    commit fa8af8424413856b9e7b9f080098124c2d71b77c
    Author: ye-yo <yeyo0x0@gmail.com>
    Date:   Mon May 9 22:59:48 2022 +0900
    
        Post: 포스트 수정
    
    commit 657640f74c0686f9986819f529ebb868298c072a
    Author: ye-yo <yeyo0x0@gmail.com>
    Date:   Mon May 9 22:40:47 2022 +0900
    
        Style: 페이지 개수 문구 위치 수정
    
    commit 25302ab92d3d92406d990c701fb92cd9075bf9e6
    Author: ye-yo <yeyo0x0@gmail.com>
    Date:   Mon May 9 22:37:49 2022 +0900
    
        Fix: 검색 모달 및 excerpt 레이아웃 수정
    
    commit 0f433af9fb26a18419b2f66f1f587868587bdd54
    Author: ye-yo <yeyo0x0@gmail.com>
    Date:   Mon May 9 22:36:16 2022 +0900
    
        Feat: Home getTimeString 함수 구현
    
    commit 15506e503515ae0a410798715b5d1278c0eadece
    Author: ye-yo <yeyo0x0@gmail.com>
    Date:   Mon May 9 22:36:08 2022 +0900
    
        Feat: Home 프로필 구현

--- _includes/scripts/home.js
+++ _includes/scripts/home.js
@@ -1,3 +1,12 @@
-/*(function () {
+(function () {
+  const time = getTimeString(new Date());
+  document.getElementById("ment").innerText = `Good ${time}`;
+})();
 
-})();*/
+function getTimeString(date) {
+  const hours = date.getHours();
+  if (hours >= 5 && hours <= 10) return "Morning! ☀️";
+  if (hours >= 11 && hours <= 17) return "Afternoon! 🏙";
+  if (hours >= 18 && hours <= 21) return "Evening! 🌉";
+  else return "Night.. 🌙";
+}
